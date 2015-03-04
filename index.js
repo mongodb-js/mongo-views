a9481555045fa9062622081ff7c29c78172bb83e
Justin J. Moses <justinjmoses@gmail.com>
2015-03-04 15:06:21 -0500

    Cleanup

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
+print('mongo-views is initiating!');
+
 if ((typeof DBView) == "undefined") {
     DBView = function(coll, name, query) {
         this._coll = coll;
         this._name = name;
         this._query = query;
-    }
+    };
 }
 
 DBView.prototype.find = function(){
     var args = [].slice.call(arguments);
     var findQuery = args.splice(0, 1)[0] || {};
-    var finalQuery = {$and:[this._query, findQuery]};
+    var finalQuery = { $and: [this._query, findQuery] };
+
     // print(this._coll + ".find(" + tojson(finalQuery) + ")");
-    return(this._coll.find({$and:[this._query, findQuery]}));
-}
+
+    return DBCollection.prototype.find.call(this._coll, finalQuery)
+};
 
 DBCollection.prototype.createView = function(name, query) {
     return(new DBView(this, name, query));
-}
+};
