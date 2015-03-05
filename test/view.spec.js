7f1f06b7ed42af1265f02b16abfa4fcec588fd6b
Justin J. Moses <justinjmoses@gmail.com>
2015-03-05 14:47:15 -0500

    Adding test structure. Run npm install

---
+++ test/view.spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// subject
+var DBView = require('../lib/views');
+
+describe('DBView', function () {
+    var view;
+    beforeEach(function () {
+        view = new DBView() ;
+    });
+    describe('todo', function () {
+        it('this is my assertion', function () {
+            // expect(...)
+        });
+    });
+});
