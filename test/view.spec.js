70cc44197803fd02e7da2bf832200a14ba76965a
Justin J. Moses <justinjmoses@gmail.com>
2015-03-05 14:51:18 -0500

    Adding sinon and chai

--- test/view.spec.js
+++ test/view.spec.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var chai = require('chai');
+var expect = chai.expect;
+var sinon = require('sinon');
+chai.use(require('sinon-chai'));
+
 // subject
 var DBView = require('../lib/views');
 
